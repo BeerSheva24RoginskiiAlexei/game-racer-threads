aecec5509ba4e9539cf5258875d29fad3771b6fb
AlexeiRoginskii <roginskiial@gmail.com>
2024-11-27 20:15:26 +0200

    hw43 definition

--- src/main/java/telran/multithreading/Race.java
+++ src/main/java/telran/multithreading/Race.java
@@ -1,49 +1,38 @@
 package telran.multithreading;
 
+import java.time.Instant;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.concurrent.locks.*;
 
 public class Race {
-    private int distance;
-    private int minSleep;
-    private int maxSleep;
-    private long startTime;
-    private List<RaceResult> results = Collections.synchronizedList(new ArrayList<>());
-
-    public Race(int distance, int minSleep, int maxSleep) {
-        this.distance = distance;
-        this.minSleep = minSleep;
-        this.maxSleep = maxSleep;
-    }
-
-    public synchronized void startRace() {
-        this.startTime = System.currentTimeMillis();
-    }
-
-    public long getStartTime() {
-        return startTime;
-    }
-
-    public int getDistance() {
-        return distance;
-    }
-
-    public int getMinSleep() {
-        return minSleep;
-    }
-
-    public int getMaxSleep() {
-        return maxSleep;
-    }
-
-    public void addResult(int racerNumber, long runningTime) {
-        synchronized (this) {
-            results.add(new RaceResult(racerNumber, runningTime));
-        }
-    }
-
-    public List<RaceResult> getResults() {
-        return results;
-    }
+	private int distance;
+	private int minSleep;
+	private int maxSleep;
+	private ArrayList<Racer> resultsTable;
+	private Instant startTime;
+	Lock lock = new ReentrantLock(true);
+	public Race(int distance, int minSleep, int maxSleep, ArrayList<Racer> resultsTable, Instant startTime) {
+		this.distance = distance;
+		this.minSleep = minSleep;
+		this.maxSleep = maxSleep;
+		this.resultsTable = resultsTable;
+		this.startTime = startTime;
+	}
+	public ArrayList<Racer> getResultsTable() {
+		return resultsTable;
+	}
+	public Instant getStartTime() {
+		return startTime;
+	}
+	
+	public int getDistance() {
+		return distance;
+	}
+	public int getMinSleep() {
+		return minSleep;
+	}
+	public int getMaxSleep() {
+		return maxSleep;
+	}
+	
 }
--- src/main/java/telran/multithreading/Racer.java
+++ src/main/java/telran/multithreading/Racer.java
@@ -1,33 +1,52 @@
 package telran.multithreading;
 
+import java.time.Instant;
 import java.util.Random;
 
 public class Racer extends Thread {
-    private Race race;
-    private int number;
+	private Race race;
+	private int number;
+	private Instant finishTime;
 
-    public Racer(Race race, int number) {
-        this.race = race;
-        this.number = number;
-    }
+	public Racer(Race race, int number) {
+		this.race = race;
+		this.number = number;
+	}
 
-    @Override
-    public void run() {
-        int minSleep = race.getMinSleep();
-        int maxSleep = race.getMaxSleep();
-        int distance = race.getDistance();
-        Random random = new Random();
+	@Override
+	public void run() {
+		int minSleep = race.getMinSleep();
+		int maxSleep = race.getMaxSleep();
+		int distance = race.getDistance();
+		Random random = new Random();
+		for (int i = 0; i < distance; i++) {
+			try {
+				sleep(random.nextInt(minSleep, maxSleep + 1));
+				System.out.printf("%d - step %d\n", number, i);
+			} catch (InterruptedException e) {
+			}
+		}
+		try {
+			race.lock.lock();
+			finishTime = Instant.now();
+			finishRace();
 
-        for (int i = 0; i < distance; i++) {
-            try {
-                sleep(random.nextInt(minSleep, maxSleep + 1));
-                System.out.printf("Racer %d - step %d\n", number, i + 1);
-            } catch (InterruptedException e) {}
-        }
+		} finally {
+			race.lock.unlock();
+		}
+	}
 
-        long finishTime = System.currentTimeMillis();
-        long runningTime = finishTime - race.getStartTime();
+	private void finishRace() {
+		race.getResultsTable().add(this);
 
-        race.addResult(number, runningTime);
-    }
+	}
+
+	public Instant getFinsishTime() {
+		return finishTime;
+
+	}
+
+	public int getNumber() {
+		return number;
+	}
 }
